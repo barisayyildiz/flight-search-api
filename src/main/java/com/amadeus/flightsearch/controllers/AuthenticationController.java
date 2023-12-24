198ef398025a0096f07e42ef3d1b967714dedfc9
barisayyildiz <99ayyldzbaris99@gmail.com>
2023-12-24 19:01:30 +0300

    feat: authentication feature implemented

---
+++ src/main/java/com/amadeus/flightsearch/controllers/AuthenticationController.java
@@ -0,0 +1,34 @@
+package com.amadeus.flightsearch.controllers;
+
+import com.amadeus.flightsearch.models.dto.AuthenticationRequest;
+import com.amadeus.flightsearch.models.dto.AuthenticationResponse;
+import com.amadeus.flightsearch.models.dto.RegisterRequest;
+import com.amadeus.flightsearch.services.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+}
